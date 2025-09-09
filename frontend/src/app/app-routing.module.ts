018962f3102900f94df2a96eecb25987b529776c
José Andres <contretaa@gmail.com>
2025-09-09 07:34:06 -0400

    initial angular project

---
+++ frontend/src/app/app-routing.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { DetailComponent } from './pages/detail/detail.component';
+
+const routes: Routes = [{ path: 'detail', component: DetailComponent }];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}

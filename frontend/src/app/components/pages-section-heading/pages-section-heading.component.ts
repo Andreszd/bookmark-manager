14025ab41c24f617a07d79e385562c5626f57182
José Andres <contretaa@gmail.com>
2025-09-23 11:25:01 -0400

    feat: register new url

--- frontend/src/app/components/pages-section-heading/pages-section-heading.component.ts
+++ frontend/src/app/components/pages-section-heading/pages-section-heading.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { PageService } from 'src/app/page.service';
+import { DropdownService } from '../dropdown/services/Dropdown.service';
 
 @Component({
   selector: 'app-pages-section-heading',
   templateUrl: './pages-section-heading.component.html',
-  styleUrls: ['./pages-section-heading.component.css']
+  styleUrls: ['./pages-section-heading.component.css'],
 })
 export class PagesSectionHeadingComponent implements OnInit {
+  pageService = inject(PageService);
+  dropdown = inject(DropdownService);
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  createPage(url?: string | null) {
+    if (url) {
+      this.pageService.create(url);
+      this.dropdown.dropdownsOpened.get('dropdown-form')?.detach();
+    }
   }
-
 }

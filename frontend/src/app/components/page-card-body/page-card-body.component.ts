c2b150079b392f865250fc70546538ca75b73300
José Andres <contretaa@gmail.com>
2025-09-30 19:12:08 -0400

    add: to allow to edit title and url

--- frontend/src/app/components/page-card-body/page-card-body.component.ts
+++ frontend/src/app/components/page-card-body/page-card-body.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterViewInit, Component, Input, OnInit } from '@angular/core';
 import { Page } from 'src/app/page.model';
 
 @Component({
@@ -6,13 +6,26 @@ import { Page } from 'src/app/page.model';
   templateUrl: './page-card-body.component.html',
   styleUrls: ['./page-card-body.component.css'],
 })
-export class PageCardBodyComponent implements OnInit {
+export class PageCardBodyComponent implements OnInit, AfterViewInit {
   @Input() small!: boolean;
   @Input() page!: Page;
 
-  constructor() {
-    console.log(this.page);
-  }
+  constructor() {}
 
   ngOnInit(): void {}
+
+  ngAfterViewInit(): void {}
+
+  catchEnter(evt: KeyboardEvent) {
+    if (evt.target && evt.key === 'Enter') {
+      this.saveChanges(
+        (evt.target as HTMLInputElement).value,
+        evt.target as HTMLInputElement
+      );
+    }
+  }
+
+  saveChanges(text: string, ref?: HTMLInputElement) {
+    ref?.blur();
+  }
 }

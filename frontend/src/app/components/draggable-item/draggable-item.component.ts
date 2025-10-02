f8489c33613840b3ac1ebfc64027798b48855785
José Andres <contretaa@gmail.com>
2025-10-02 10:44:46 -0400

    disable some actions when has cards selected

--- frontend/src/app/components/draggable-item/draggable-item.component.ts
+++ frontend/src/app/components/draggable-item/draggable-item.component.ts
@@ -7,14 +7,35 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 })
 export class DraggableItemComponent implements OnInit {
   @Input() id!: string | number;
+  @Input() gosthImgUrl?: string;
+  @Input() isDraggable = true;
   @Output() onDragStart = new EventEmitter<DragEvent>();
+  gosthImg = new Image();
+  isDragging = false;
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    if (this.gosthImgUrl) {
+      this.gosthImg.src = this.gosthImgUrl;
+      this.gosthImg.onload = () => {};
+    }
+  }
 
   handleOnDragStart(event: DragEvent) {
+    if (!this.isDraggable) return;
+
+    this.isDragging = true;
     this.onDragStart.emit();
+
+    if (this.gosthImgUrl) {
+      event.dataTransfer?.setDragImage(this.gosthImg, -5, -5);
+    }
+
     event.dataTransfer?.setData('id', this.id?.toString());
+  }
+
+  handleOnDragEnd(event: DragEvent) {
+    this.isDragging = false;
   }
 }

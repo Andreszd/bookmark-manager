83ab4783b702950a2bb975f5f45a9dda9d2ee3f4
José Andres <contretaa@gmail.com>
2025-10-07 19:50:25 -0400

    without groups & trash modules added

---
+++ frontend/src/app/pages/trash/trash.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'trash',
+  templateUrl: './trash.component.html',
+  styleUrls: ['./trash.component.css'],
+})
+export class TrashComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}

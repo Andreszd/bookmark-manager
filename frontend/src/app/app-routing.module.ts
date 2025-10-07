d2e2c73894d8db8ce36b174925d05204cbdeb310
José Andres <contretaa@gmail.com>
2025-10-07 19:39:39 -0400

    routing added

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DetailComponent } from './pages/detail/detail.component';
+import { WithoutGroupsComponent } from './pages/without-groups/without-groups.component';
+import { TrashComponent } from './pages/trash/trash.component';
 
-const routes: Routes = [{ path: 'detail', component: DetailComponent }];
+const routes: Routes = [
+  { path: '', component: WithoutGroupsComponent },
+  { path: 'trash', component: TrashComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

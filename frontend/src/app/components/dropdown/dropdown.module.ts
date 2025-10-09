d0cd37cec74a3033eae75080349732aa3a2f667b
José Andres <contretaa@gmail.com>
2025-10-09 13:06:59 -0400

    dropdown container added

--- frontend/src/app/components/dropdown/dropdown.module.ts
+++ frontend/src/app/components/dropdown/dropdown.module.ts
@@ -3,11 +3,20 @@ import { CommonModule } from '@angular/common';
 import { DropdownService } from './services/Dropdown.service';
 import { DropdownContentDirective } from './directives/DropdownContentDirective';
 import { DropdownOriginDirective } from './directives/DropdownOriginDirective';
+import { DropdownContainerComponent } from './components/dropdown-container/dropdown-container.component';
 
 @NgModule({
-  declarations: [DropdownContentDirective, DropdownOriginDirective],
+  declarations: [
+    DropdownContentDirective,
+    DropdownOriginDirective,
+    DropdownContainerComponent,
+  ],
   imports: [CommonModule],
   providers: [DropdownService],
-  exports: [DropdownContentDirective, DropdownOriginDirective],
+  exports: [
+    DropdownContentDirective,
+    DropdownOriginDirective,
+    DropdownContainerComponent,
+  ],
 })
 export class DropdownModule {}

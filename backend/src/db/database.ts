578dea0c31baccfbedb0f45899d856a24fbfcafe
José Andres <contretaa@gmail.com>
2025-10-14 10:54:55 -0400

    database connection

---
+++ backend/src/db/database.ts
@@ -0,0 +1,47 @@
+import { Db, DbOptions, MongoClient } from "mongodb";
+import { UrlModel } from "../models/url.model";
+import { GroupModel } from "../models/group.model";
+
+const CONNECTION_URI = "mongodb://127.0.0.1:27017/bookmark";
+
+export class Database {
+  static connection?: MongoClient;
+  static operations?: Db;
+
+  constructor() {}
+
+  createCollections() {
+    if (Database.operations) {
+      Database.operations
+        .createCollection("url", {
+          validator: {
+            $jsonSchema: UrlModel.jsonScheme,
+          },
+        })
+        .catch(() => {});
+
+      Database.operations
+        .createCollection("group", {
+          validator: {
+            $jsonSchema: GroupModel.jsonScheme,
+          },
+        })
+        .catch(() => {});
+
+      console.log("created collections");
+    }
+  }
+
+  run() {
+    if (!Database.connection) {
+      const client = new MongoClient(CONNECTION_URI);
+      client.connect().then((connection) => {
+        Database.connection = connection;
+        Database.operations = connection.db("bookmark-project");
+
+        this.createCollections();
+        console.log("database connected");
+      });
+    }
+  }
+}

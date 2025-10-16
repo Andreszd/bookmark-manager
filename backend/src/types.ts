f5727e3eb6472943eaf44f5f3a8894386691ddff
José Andres <contretaa@gmail.com>
2025-10-16 17:02:51 -0400

    add new types to omit some data and add user in request type

--- backend/src/types.ts
+++ backend/src/types.ts
@@ -1,1 +1,12 @@
+import { Request } from 'express';
+import { JwtPayload } from 'jsonwebtoken';
+
 export type OmitId<T> = Omit<T, '_id'>;
+
+export type OmitGenData<T> = Omit<T, '_id' | 'createdAt'>;
+
+export type OmitIds<T> = Omit<T, '_id' | 'userId' | 'groupId'>;
+
+export interface AuthRequest extends Request {
+  user?: string | JwtPayload;
+}

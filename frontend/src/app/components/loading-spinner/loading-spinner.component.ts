e1d719291f37d4ab37fe0699a3259f65452b3932
José Andres <contretaa@gmail.com>
2025-10-21 11:57:12 -0400

    add loading spinner comp

---
+++ frontend/src/app/components/loading-spinner/loading-spinner.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'loading-spinner',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './loading-spinner.component.html',
+  styleUrls: ['./loading-spinner.component.css'],
+})
+export class LoadingSpinnerComponent implements OnInit {
+  @Input() size: string | number = '40px';
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}

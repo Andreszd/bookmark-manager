15a762b21a786cf74b64cb1bbb96a028b071c602
José Andres <contretaa@gmail.com>
2025-10-23 21:44:21 -0400

    add: intergrate page creation api

--- frontend/src/app/pages/pages/components/pages-section-heading/pages-section-heading.component.ts
+++ frontend/src/app/pages/pages/components/pages-section-heading/pages-section-heading.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject, OnInit, ViewChild } from '@angular/core';
 import { DropdownService } from '../../../../shared/components/dropdown/services/Dropdown.service';
 import { DropdownOriginDirective } from '../../../../shared/components/dropdown/directives/DropdownOriginDirective';
+import { PageService } from '../../services/page.service';
 
 @Component({
   selector: 'pages-section-heading',
@@ -9,14 +10,18 @@ import { DropdownOriginDirective } from '../../../../shared/components/dropdown/
 })
 export class PagesSectionHeadingComponent implements OnInit {
   dropdown = inject(DropdownService);
-  @ViewChild('#dref') dropdownFormRef!: DropdownOriginDirective;
+  pageService = inject(PageService);
+  @ViewChild('dref') dropdownFormRef!: DropdownOriginDirective;
   constructor() {}
 
   ngOnInit(): void {}
 
   createPage(url?: string | null) {
     if (url) {
-      this.dropdown.dropdownsOpened.get(this.dropdownFormRef)?.detach();
+      this.pageService.create(url).subscribe(() => {
+        this.dropdown.dropdownsOpened.get(this.dropdownFormRef)?.detach();
+        this.pageService.getAll();
+      });
     }
   }
 }

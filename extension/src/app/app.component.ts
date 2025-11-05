9fa5cf41901cbf26ae1c5918833534066119a0a0
José Andres <contretaa@gmail.com>
2025-11-05 11:39:21 -0400

    add: base ui for login and save urls

--- extension/src/app/app.component.ts
+++ extension/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AuthService } from './shared/services/auth.service';
+import { BookmarkService } from './shared/services/bookmark.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'extension';
+  authService = inject(AuthService);
+  bookmarkService = inject(BookmarkService);
+
+  constructor() {}
+
+  login() {
+    chrome.windows.create({
+      url: 'http://localhost:4200/auth?login_from_extension=true',
+      type: 'popup',
+      width: 500,
+      height: 600,
+      left: screen.width / 2 - 250,
+      top: screen.height / 2 - 300,
+    });
+  }
 }
--- extension/src/app/app.module.ts
+++ extension/src/app/app.module.ts
@@ -3,16 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BookmarkService } from './shared/services/bookmark.service';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { UrlInterceptor } from './shared/http/interceptors/url-interceptor.http';
+import { AuthInterceptor } from './shared/http/interceptors/auth.interceptor';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  providers: [
+    BookmarkService,
+    { provide: HTTP_INTERCEPTORS, useClass: UrlInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}

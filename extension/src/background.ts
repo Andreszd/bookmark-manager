ea9ebf9af12b072eda731e2900cea6dfc099b3f5
José Andres <contretaa@gmail.com>
2025-11-05 11:38:03 -0400

    update: listener to catch messages sent by web

--- extension/src/background.ts
+++ extension/src/background.ts
@@ -10,3 +10,20 @@ chrome.runtime.onInstalled.addListener(() => {
     { url: [{ hostContains: 'google.com' }] }
   );
 });
+
+chrome.runtime.onMessageExternal.addListener(
+  (message, sender, sendResponse) => {
+    if (message.token) {
+      chrome.storage.local.set({ token: message.token }, () => {
+        sendResponse({ status: 'success' });
+      });
+
+      sendResponse({ status: 'success' });
+
+      chrome.runtime.sendMessage({
+        event: 'token_received',
+        token: message.token,
+      });
+    }
+  }
+);

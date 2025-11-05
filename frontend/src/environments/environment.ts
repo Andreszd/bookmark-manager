c2441acc6c832b181ce1967460a172ab75ec3572
José Andres <contretaa@gmail.com>
2025-11-05 11:51:28 -0400

    add extensionId in env

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -6,6 +6,7 @@ export const environment = {
   production: false,
   imgsBucketUrl: 'http://localhost:3000',
   apiUrl: 'http://localhost:3000/api',
+  extensionId: 'mddeemoanplmekgocpfjfmminfimdiga',
 };
 
 /*
